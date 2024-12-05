160a760bff022881db71e9decb864d4ba028fe69
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-05 21:23:43 +0530

    made few changes based on netlify recommendation

--- src/Components/Charts.jsx
+++ src/Components/Charts.jsx
@@ -1,153 +1,160 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from "react";
 import {
-  Line,
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  ResponsiveContainer,
-  Tooltip,
-  Legend,
+	Line,
+	LineChart,
+	CartesianGrid,
+	XAxis,
+	YAxis,
+	ResponsiveContainer,
+	Tooltip,
+	Legend,
 } from "recharts";
 import { CryptoContext } from "../context/CryptoContext";
 
 function ChartComponent({ data, currency, type }) {
-  return (
-    <ResponsiveContainer height={"90%"}>
-      <LineChart width={400} height={400} data={data}>
-        <Line
-          type="monotone"
-          dataKey= {type}
-          stroke="#14ffec"
-          strokeWidth="1px"
-        />
-        <CartesianGrid stroke="#323232" />
-        <Tooltip
-          content={<CustomTooltip />}
-          currency={currency}
-          cursor={false}
-          wrapperStyle={{ outline: "none" }}
-        />
-        <Legend />
-        <XAxis dataKey="date" hide />
-        <YAxis data={type} hide domain={["auto", "auto"]} />
-      </LineChart>
-    </ResponsiveContainer>
-  );
+	return (
+		<ResponsiveContainer height={"90%"}>
+			<LineChart width={400} height={400} data={data}>
+				<Line
+					type="monotone"
+					dataKey={type}
+					stroke="#14ffec"
+					strokeWidth="1px"
+				/>
+				<CartesianGrid stroke="#323232" />
+				<Tooltip
+					content={<CustomTooltip />}
+					currency={currency}
+					cursor={false}
+					wrapperStyle={{ outline: "none" }}
+				/>
+				<Legend />
+				<XAxis dataKey="date" hide />
+				<YAxis data={type} hide domain={["auto", "auto"]} />
+			</LineChart>
+		</ResponsiveContainer>
+	);
 }
 
 function CustomTooltip({ payload, label, active, currency = "usd" }) {
-  if (active && payload && payload.length > 0) {
-    return (
-      <div className="custom-tooltip">
-        <p className="label text-sm text-cyan">{`${label} : ${new Intl.NumberFormat(
-          "en-IN",
-          {
-            style: "currency",
-            currency: currency,
-            minimumFractionDigits: 5,
-          }
-        ).format(payload[0].value)}`}</p>
-      </div>
-    );
-  }
+	if (active && payload && payload.length > 0) {
+		return (
+			<div className="custom-tooltip">
+				<p className="label text-sm text-cyan">{`${label} : ${new Intl.NumberFormat(
+					"en-IN",
+					{
+						style: "currency",
+						currency: currency,
+						minimumFractionDigits: 5,
+					}
+				).format(payload[0].value)}`}</p>
+			</div>
+		);
+	}
 }
 
 export default function Charts({ id }) {
-  const { currency } = useContext(CryptoContext);
-  const [chartData, setChartData] = useState(null);
-  const [type, setType] = useState("prices");
-  const [days, setDays] = useState(7);
+	const { currency } = useContext(CryptoContext);
+	const [chartData, setChartData] = useState(null);
+	const [type, setType] = useState("prices");
+	const [days, setDays] = useState(7);
 
-  async function getChartData(id) {
-    const data = await fetch(
-      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`
-    )
-      .then((res) => res.json())
-      .then((json) => json)
-      .catch((error) => console.log(error));
+	async function getChartData(id) {
+		const data = await fetch(
+			`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`
+		)
+			.then((res) => res.json())
+			.then((json) => json)
+			.catch((error) => console.log(error));
 
-    // console.log("chart-data", data);
+		// console.log("chart-data", data);
 
-    const convertedData = data[type].map((item) => {
-      return {
-        date: new Date(item[0]).toLocaleDateString(),
-        [type]: item[1],
-      };
-    });
+		const convertedData = data[type].map((item) => {
+			return {
+				date: new Date(item[0]).toLocaleDateString(),
+				[type]: item[1],
+			};
+		});
 
-    //   console.log(convertedData)
+		//   console.log(convertedData)
 
-    setChartData(convertedData);
-  }
+		setChartData(convertedData);
+	}
 
-  useEffect(
-    function () {
-      getChartData(id);
-    },
-    [id, type, days]
-  );
+	useEffect(
+		function () {
+			getChartData(id);
+		},
+		[id, type, days]
+	);
 
-  return (
-    <div className="w-full h-[60%]">
-      <ChartComponent data={chartData} currency={currency} type={type} />
-      <div className="flex">
-        <button
-          className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
-            type === "prices"
-              ? "bg-cyan text-cyan"
-              : "bg-gray-200 text-gray-100"
-          }`}
-          onClick={() => setType("prices")}
-        >
-          Price
-        </button>
-        <button
-          className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
-            type === "market_caps"
-              ? "bg-cyan text-cyan"
-              : "bg-gray-200 text-gray-100"
-          }`}
-          onClick={() => setType("market_caps")}
-        >
-          market caps
-        </button>
-        <button
-          className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
-            type === "total_volumes"
-              ? "bg-cyan text-cyan"
-              : "bg-gray-200 text-gray-100"
-          }`}
-          onClick={() => setType("total_volumes")}
-        >
-          total volumes
-        </button>
+	return (
+		<div className="w-full h-[60%]">
+			<ChartComponent data={chartData} currency={currency} type={type} />
+			<div className="flex">
+				<button
+					className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
+						type === "prices"
+							? "bg-cyan text-cyan"
+							: "bg-gray-200 text-gray-100"
+					}`}
+					onClick={() => setType("prices")}
+				>
+					Price
+				</button>
+				<button
+					className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
+						type === "market_caps"
+							? "bg-cyan text-cyan"
+							: "bg-gray-200 text-gray-100"
+					}`}
+					onClick={() => setType("market_caps")}
+				>
+					market caps
+				</button>
+				<button
+					className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
+						type === "total_volumes"
+							? "bg-cyan text-cyan"
+							: "bg-gray-200 text-gray-100"
+					}`}
+					onClick={() => setType("total_volumes")}
+				>
+					total volumes
+				</button>
 
-        <button
-          className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
-            days === 7 ? "bg-cyan text-cyan" : "bg-gray-200 text-gray-100"
-          }`}
-          onClick={() => setDays(7)}
-        >
-          7d
-        </button>
-        <button
-          className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
-            days === 14 ? "bg-cyan text-cyan" : "bg-gray-200 text-gray-100"
-          }`}
-          onClick={() => setDays(14)}
-        >
-          14d
-        </button>
-        <button
-          className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
-            days === 30 ? "bg-cyan text-cyan" : "bg-gray-200 text-gray-100"
-          }`}
-          onClick={() => setDays(30)}
-        >
-          30d
-        </button>
-      </div>
-    </div>
-  );
+				<button
+					className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
+						days === 7
+							? "bg-cyan text-cyan"
+							: "bg-gray-200 text-gray-100"
+					}`}
+					onClick={() => setDays(7)}
+				>
+					7d
+				</button>
+				<button
+					className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
+						days === 14
+							? "bg-cyan text-cyan"
+							: "bg-gray-200 text-gray-100"
+					}`}
+					onClick={() => setDays(14)}
+				>
+					14d
+				</button>
+				<button
+					className={`text-sm py-0.5 px-1.5 ml-2 bg-opacity-25 rounded capitalize ${
+						days === 30
+							? "bg-cyan text-cyan"
+							: "bg-gray-200 text-gray-100"
+					}`}
+					onClick={() => setDays(30)}
+				>
+					30d
+				</button>
+			</div>
+		</div>
+	);
 }
